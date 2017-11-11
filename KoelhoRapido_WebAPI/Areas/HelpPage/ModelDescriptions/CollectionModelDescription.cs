c966e90cde377a13acb8a3f1e4b10d67aeee6a65
felipeferrazdeveloper <felipeferrazdeveloper@users.noreply.github.com>
2017-11-11 13:29:02 -0200

    Add project files.

---
+++ KoelhoRapido_WebAPI/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace KoelhoRapido_WebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ KoelhoRapido_WebAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace KoelhoRapido_WebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
